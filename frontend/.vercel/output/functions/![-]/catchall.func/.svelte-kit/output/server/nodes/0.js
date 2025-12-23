0421764c73551ff9f38ba4fa3be0ca6d3d32130b
navin-chaudhary <heynavin.im@gmail.com>
2025-12-23 10:57:32 +0530

    chnages versel related

--- frontend/.vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.BzCw8O8M.js","_app/immutable/chunks/C4KTUKtR.js","_app/immutable/chunks/hSOCBuBK.js"];
-export const stylesheets = ["_app/immutable/assets/0.BMrV5yC_.css"];
-export const fonts = [];
